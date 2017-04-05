721db547de5e60af46a5a1b6bf22950d7cb653c5
Ash McKenzie <ash@the-rebellion.net>
2017-04-06 07:24:37 +1000

    Preparing for Travis

--- spec/freshcuts_spec.rb
+++ spec/freshcuts_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Freshcuts do
   it "has a version number" do
     expect(Freshcuts::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
